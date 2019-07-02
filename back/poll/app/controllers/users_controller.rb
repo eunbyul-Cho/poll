807b10d53da3989a87d11bbdf1d7b5a786ca0339
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-02 14:48:31 +0900

    save UserName from input

--- back/poll/app/controllers/users_controller.rb
+++ back/poll/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def show
+    @currentUser = User.find_by(id:params[:id])
+    if !@currentUser
+      render text:"noUser"
+    else
+    render json: @currentUser.votes
+    end
+  end
+
   def create
      User.create(name:params[:name])
      render text: params[:name]
