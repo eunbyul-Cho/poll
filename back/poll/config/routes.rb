e97772995e9d07d3fe7521760975948968200ef4
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-04 15:10:10 +0900

    polls.candidates controller working

--- back/poll/config/routes.rb
+++ back/poll/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :polls do
-    resources :candidates,controller: "polls/candidate", as: :vote
+    resources :candidates,controller: "polls/candidates", as: :vote
   end
   resources :users do
     get '/polls', to: "users#show"
