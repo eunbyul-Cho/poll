b638faf14b5dbd9b1109204d1242524fa0a2868e
Eunbyul Cho <hello@eunbyulcho.me>
2019-07-04 15:04:45 +0900

    change route

--- back/poll/config/routes.rb
+++ back/poll/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :polls
-    put 'polls/:id', to: 'polls#update', as: :vote
+  resources :polls do
+    resources :candidates, to: 'polls/candidates#update', as: :vote
+  end
   resources :users do
     get '/polls', to: "users#show"
   end
